abb0d1a59dc4736f893166a83b0a67b86b9c4829
WebMoss <craigmoss36@gmail.com>
2021-06-15 12:04:17 +0200

    Trying npm generate

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,6 +75,7 @@ export default {
         config: {
           apiKey: process.env.FIREBASE_API_KEY,
           authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          databaseURL: process.env.FIREBASE_DATABASEURL,
           projectId: process.env.FIREBASE_PROJECT_ID,
           storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
           messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
@@ -95,6 +96,8 @@ export default {
             // emulatorHost: 'http://localhost',
           },
           firestore: true,
+          storage: true,
+          performance: true,
           analytics: false
         },
       },
